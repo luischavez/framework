b48b1ef105f328f8bae18c2ffc97fb8db997a881
Luis <Frost_Leviathan@hotmail.com>
2018-09-03 22:27:44 +0000

    updated encrypter

--- src/Illuminate/Encryption/EncryptionServiceProvider.php
+++ src/Illuminate/Encryption/EncryptionServiceProvider.php
@@ -11,17 +11,20 @@ class EncryptionServiceProvider extends ServiceProvider {
 	 */
 	public function register()
 	{
-		$this->app->bindShared('encrypter', function($app)
-		{
-			$encrypter =  new Encrypter($app['config']['app.key']);
+        $this->app->bindShared('encrypter', function($app)
+        {
+            if ($app['config']->has('app.cipher')) {
+                return new Encrypter(
+                    $app['config']['app.key'],
+                    $app['config']['app.cipher']
+                );
+            }
+            else {
+                return new Encrypter($app['config']['app.key']);
+            }
+        });
 
-			if ($app['config']->has('app.cipher'))
-			{
-				$encrypter->setCipher($app['config']['app.cipher']);
-			}
-
-			return $encrypter;
-		});
+        $this->app->alias('encrypter', Encrypter::class);
 	}
 
 }
